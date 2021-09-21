0b4977527c170dcd90b4821fe7f29c4f436e38b6
Pete Sahat <Pete Sahat@DESKTOP-CJD7L86>
2021-09-21 01:54:36 -0700

    added name to line 11

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("301567890 - This line was added by Luis Lujan.");
 		System.out.println("302575507 - This line was added by Rick Ammann.");
 		System.out.println("302255200 - This line was added by Dave Kaercher.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301361840 - This line was added by Andrew Abella.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
