f824f51893c0e0d34bb8328dd7a65b8e619ccfe5
jia13 <jia13@DESKTOP-OA98S1D>
2021-09-21 17:36:15 -0700

    added/changed to line 12

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
 		System.out.println("302575507 - This line was added by Rick Ammann.");
 		System.out.println("302255200 - This line was added by Dave Kaercher.");
 		System.out.println("301361840 - This line was added by Andrew Abella.");
+		System.out.println("220024169 - This line was added by Jiawei Li.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
